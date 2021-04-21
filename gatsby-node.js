d872c1ac9e72ea543100cc24a3a27ddb1b67b85d
ntlaseca <nate.tlaseca@gmail.com>
2021-04-20 20:37:22 -0500

    reconfigure to use new packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,13 @@
-exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+const path = require('path');
+const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  getConfig,
+  rules,
+  loaders,
+  actions,
+}) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
       module: {
@@ -11,47 +20,12 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       }
     })
   }
-}
-
-exports.createPages = async ({ actions, graphql, reporter }) => {
-  const result = await graphql(`
-    {
-      allProjectsJson {
-        edges {
-          node {
-            slug
-          }
-        }
-      }
-    }
-  `)
-
-  if (result.error) {
-    reporter.panin('There was a problem loading your projects.')
-    return
-  }
-  
-  const projects = result.data.allProjectsJson.edges;
-
-  projects.forEach(({ node: {slug} }, index) => {
-    actions.createPage({
-      path: `/${slug}/`,
-      component: require.resolve('./src/templates/project.js'),
-      context: { 
-        slug: `${slug}`,
-        relativeDirectory: `images/projects/${slug}`,
-        prev: index === 0 ? projects[projects.length - 1].node : projects[index - 1].node,
-        next: index === (projects.length - 1) ? projects[0].node : projects[index + 1].node
-      }
-    })
-  })
-}
-
-const pathsToIgnore = ['/tenantu', '/horn']
-
-exports.onCreatePage = ({ page, actions }) => {
-  if (pathsToIgnore.includes(page.path)) {
-    const { deletePage } = actions
-    deletePage(page)
-  }
-}
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      plugins: [new DirectoryNamedWebpackPlugin({
+        exclude: /node_modules/
+      })],
+    },
+  });
+};
--- site-config.js
+++ site-config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  siteTitle: `Nate Tlaseca - Portfolio`,
-  siteTitleShort: `Nate Tlaseca`,
+  siteTitle: `Nate Tlaseca`,
   siteDescription: `Nate Tlaseca is a human and creative web developer from Delaware.`,
   siteUrl: `https://natetlaseca.com`,
+  siteAuthor: `Nate Tlaseca`,
   themeColor: `#6a62fa`,
   backgroundColor: `#e6e6e6`,
   pathPrefix: null,
