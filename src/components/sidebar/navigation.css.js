208cb30c13f64dcce6d60012fed95f860c7a0a10
ntlaseca <nate.tlaseca@gmail.com>
2021-04-22 10:27:39 -0500

    make usable in template and regular pages

---
+++ src/components/sidebar/navigation.css.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 4.8rem;
+`
+
+export const MobileWrapper = styled.div`
+  grid-column: span 12;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 4.8rem;
+`
