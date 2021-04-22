79b8e5f01ef411d636f7bb6cb95a6b529473e088
ntlaseca <nate.tlaseca@gmail.com>
2021-04-22 10:25:35 -0500

    add meta query to layout component

--- src/components/layout/layout.css.js
+++ src/components/layout/layout.css.js
@@ -3,3 +3,10 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   padding: 3.2rem;
 `
+
+export const Content = styled.div`
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: auto;
+  grid-gap: 1em;
+`
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,27 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
-import { Wrapper } from "./layout.css";
+import React from "react"
+import PropTypes from "prop-types"
+import { StaticQuery, graphql } from "gatsby"
+import { Wrapper, Content } from "./layout.css"
 import Head from "components/head"
-import Header from "components/header";
-import Footer from "components/footer";
-import GlobalStyle from "styles/global.css";
+import Header from "components/header"
+import Footer from "components/footer"
+import GlobalStyle from "styles/global.css"
 
 const Layout = ({ children }) => (
   <Wrapper>
     <GlobalStyle />
     <Head />
     <Header />
-    <main className="d-grid">
-      {children}
-    </main>
+    <Content>{children}</Content>
     <Footer />
   </Wrapper>
-);
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-};
+}
 
 const LayoutWithQuery = props => (
   <StaticQuery
@@ -36,10 +34,10 @@ const LayoutWithQuery = props => (
     `}
     render={data => <Layout data={data} {...props} />}
   />
-);
+)
 
 LayoutWithQuery.propTypes = {
   children: PropTypes.node.isRequired,
-};
+}
 
-export default Layout
+export default LayoutWithQuery
