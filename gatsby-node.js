97cbd55b95fdeca3a5fe03b6c609e3ddf1cc7aa3
ntlaseca <nate.tlaseca@gmail.com>
2021-04-24 16:29:49 -0500

    add createPages (for now)

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,3 +29,46 @@ exports.onCreateWebpackConfig = ({
     },
   });
 };
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    {
+      allProjectsJson {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  if (result.error) {
+    reporter.panin('There was a problem loading your projects.')
+    return
+  }
+  
+  const projects = result.data.allProjectsJson.edges;
+
+  projects.forEach(({ node: {slug} }, index) => {
+    actions.createPage({
+      path: `/${slug}/`,
+      component: require.resolve('./src/templates/project.js'),
+      context: { 
+        slug: `${slug}`,
+        relativeDirectory: `images/projects/${slug}`,
+        prev: index === 0 ? projects[projects.length - 1].node : projects[index - 1].node,
+        next: index === (projects.length - 1) ? projects[0].node : projects[index + 1].node
+      }
+    })
+  })
+}
+
+const pathsToIgnore = ['/tenantu', '/horn']
+
+exports.onCreatePage = ({ page, actions }) => {
+  if (pathsToIgnore.includes(page.path)) {
+    const { deletePage } = actions
+    deletePage(page)
+  }
+}
