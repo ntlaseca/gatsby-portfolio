97666a9ba381d7ae8ff75741198f0ac0c4dab4c0
ntlaseca <nate.tlaseca@gmail.com>
2021-04-26 15:14:59 -0500

    correct imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import Layout from "components/layout"
 import About from "components/about/about"
-import Sketch from "components/about/sketch"
-import sketch from "components/about/self-portrait"
+import SelfPortrait from "components/self-portrait/self-portrait"
+import sketch from "components/self-portrait/sketch"
 import Main from "components/main"
 import ProjectList from "components/project-list/project-list"
 
 const IndexPage = props => {
   return (
     <Layout>
-      <Sketch sketch={sketch} />
+      <SelfPortrait sketch={sketch} />
       <Main>
         <About />
         <ProjectList />
