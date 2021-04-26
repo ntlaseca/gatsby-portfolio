73357c0cf4c59e3e5b6c28d9ddd2de0a195d757b
ntlaseca <nate.tlaseca@gmail.com>
2021-04-26 15:14:52 -0500

    improve spacing

--- src/components/sidebar/sidebar.css.js
+++ src/components/sidebar/sidebar.css.js
@@ -3,7 +3,7 @@ import MEDIA from "helpers/mediaTemplates"
 
 export const Wrapper = styled.aside`
   position: fixed;
-  max-width: calc(33.33333% - 3.6rem);
+  max-width: calc(33.33333% - 7.2rem);
 
   & > div {
     grid-column: span 12;
