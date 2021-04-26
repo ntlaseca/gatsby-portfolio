ff506d151c4f1131302b0666f2c76b344739aed2
ntlaseca <nate.tlaseca@gmail.com>
2021-04-26 15:14:12 -0500

    simplify formatting

--- src/components/footer/footer.css.js
+++ src/components/footer/footer.css.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.footer`
   margin-top: 6.4rem;
+  font-size: 2.8rem;
 `
 
 export const Row = styled.div`
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,12 +4,8 @@ import { Wrapper, Row } from "./footer.css"
 const Footer = () => (
   <Wrapper>
     <Row>
-      <div>
-        © {new Date().getFullYear()}
-      </div>
-      <div>
-        <a href="mailto:nate.tlaseca@gmail.com?subject=Hey,%20Nate!" target="_blank" rel="noopener noreferrer">Email me!</a>
-      </div>
+      <span>© {new Date().getFullYear()}</span>
+      <a href="mailto:nate.tlaseca@gmail.com?subject=Hey,%20Nate!" target="_blank" rel="noopener noreferrer">Email me!</a>
     </Row>
   </Wrapper>
 )
