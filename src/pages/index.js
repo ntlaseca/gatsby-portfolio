f931290693adf3d8dd0a7a1df9e96ad213a172fe
ntlaseca <nate.tlaseca@gmail.com>
2021-04-30 12:21:05 -0500

    add proper Head props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Head from "components/head"
 import Layout from "components/layout"
 import About from "components/about/about"
 import SelfPortrait from "components/self-portrait/self-portrait"
@@ -6,9 +8,28 @@ import sketch from "components/self-portrait/sketch"
 import Main from "components/main"
 import ProjectList from "components/project-list/project-list"
 
-const IndexPage = props => {
+export const HomepageQuery = graphql`
+  query {
+    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+      title
+      description
+      keywords
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const title = data.site.title
+  const description = data.site.description
+  const keywords = data.site.keywords
+
   return (
     <Layout>
+      <Head 
+        title={title} 
+        description={description} 
+        keywords={keywords} 
+      />
       <SelfPortrait sketch={sketch} />
       <Main>
         <About />
