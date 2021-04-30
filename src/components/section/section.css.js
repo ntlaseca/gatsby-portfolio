a792ad940ebc7d16e0e14e9143c5557c2a752043
ntlaseca <nate.tlaseca@gmail.com>
2021-04-30 12:19:48 -0500

    make gifs full width

--- src/components/section/section.css.js
+++ src/components/section/section.css.js
@@ -2,4 +2,9 @@ import styled from "styled-components"
 
 export const Wrapper = styled.section`
   grid-column: span ${props => props.span};
+
+  & > img {
+    display: block;
+    width: 100%;
+  }
 `
