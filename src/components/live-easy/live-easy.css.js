da1e71681e63cff9c4a5199178503a7b4af77c90
ntlaseca <nate.tlaseca@gmail.com>
2021-04-30 18:54:29 -0500

    refactor component for React

--- src/components/live-easy/live-easy.css.js
+++ src/components/live-easy/live-easy.css.js
@@ -1,32 +1,30 @@
-@import "../../styles/scss/animations.scss";
+import styled from "styled-components"
 
-/*
- * keyframes for "Live easy."
- */
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 12.8rem;
+  padding-bottom: 12.8rem;
 
-@include keyframes(floating-text) {
-  0% {
-    transform: translateY(.15em);
-    text-shadow:0 0 .1em rgba(255,255,255,.6),
-      0 0 .1em rgba(0,0,0,.1),
-      0 .1em .2em rgba(0,0,0,.2);
+  & > div {
+    width: 80%;
 
+    & > svg {
+      @keyframes floating-text {
+        0% {
+          transform: translateY(3em);
+          filter: drop-shadow(
+            0 .5em .5em rgba(0,0,0,.2)
+          );
+        }
+        100% {
+          filter: drop-shadow(
+            0 2.5em 2.5em rgba(0,0,0,.1)
+          );
+        }
+      }
+      animation: floating-text 4s ease-in-out infinite alternate;
+    }
   }
-  100% {
-    text-shadow:0 0 .4em rgba(255,255,255,.1),
-      0 0 .3em rgba(0,0,0,.1),
-      0 .3em .5em rgba(0,0,0,.1);
-  }
-}
-
-.floating {
-  font-family: 'Metropolis', sans-serif;
-  font-weight: 700;
- 	font-size: 12vw;
- 	letter-spacing: -.025em;
- 	color: #369AFF;
-  padding-top: 1.5em;
- 	padding-bottom: 1.2em;
- 	margin-bottom: 0;
- 	@include animation('floating-text 4s ease-in-out alternate infinite');
-}
+`
