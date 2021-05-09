5b8256abd6c913738e6914b96e5d9ee18ae06d50
ntlaseca <nate.tlaseca@gmail.com>
2021-05-09 15:31:01 -0500

    revised bio

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -3,7 +3,7 @@ import { Bio } from "./about.css"
 
 const About = () => (
   <div>
-    <Bio>Nate Tlaseca is a web developer and designer with an affection for the internet, digital media, and technology. He is a Delawarean and currently resides in Dallas, TX.</Bio>
+    <Bio>Nate Tlaseca is a Dallas-based web developer and designer with an affection for the internet, digital media, and technology.</Bio>
   </div>
 )
 
